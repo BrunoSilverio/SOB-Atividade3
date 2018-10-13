27b4efc66983b9fecca0e1f08bab80b0f67af2f3
guipernicone <guipernicone@hotmail.com>
2018-10-13 17:15:33 -0300

    script para atualizar modulo

--- Projeto1Crypto/cryptomodule.h
+++ Projeto1Crypto/cryptomodule.h
@@ -7,22 +7,35 @@
  *
  */
 /*Ver o que cada biblioteca faz ou representa e comentar*/
-#include <linux/kernel.h>         //Necessario para para fazer a build do module e função printk
-#include <linux/init.h>           //Necessario para usar macro das funções init_module e cleanup_module...
-#include <linux/module.h>         //Necessario para todo modulo de kernel
-#include <linux/fs.h>             //para usar file_operations
-#include <linux/uaccess.h>	      //Para acessar a memoria do usuario.
-#include <linux/errno.h>          //biblioteca para exibir os erros
-#include <linux/miscdevice.h>     //Permite o uso de um misc device(declaração de um driver novo)
-#include <linux/slab.h>           //Permite o uso do kmalloc e do kfree
-#include <linux/crypto.h>         //biblioteca para encriptar
-#include <linux/device.h>         //controle de um dispositivo
-#include <linux/cdev.h>		        //Dispositivo do tipo de caracteres, usado para gravar-lo no sistema e identificar o dispositivo.
-#include <linux/irq.h>		        //Interrupção a qual a notificação se aplica.
-#include <asm/uaccess.h>	        //Acesso do usuario (ativando/desativando) macros.
-#include <asm/irq.h>		          //
-#include <asm/io.h>		            //Genérico IO read/write. Executa acesso Native-Endian.
-#include <linux/poll.h>		        //wait for some event on a file descriptor.
+
+/*Bibliotecas Necessarias*/
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/crypto.h>
+
+/*Vr se precisa*/
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
+#include <linux/irq.h>
+#include <asm/uaccess.h>
+#include <asm/irq.h>
+#include <asm/io.h>
+#include <linux/poll.h>
+#include <linux/scatterlist.h>
+
+
+#include <crypto/aead.h>
+#include <linux/gfp.h>
+#include <linux/types.h>
+#include <crypto/internal/skcipher.h>
+#include <asm/unistd.h>
 
 #define DEVICE_NAME "cryptomodule"
 #define DISK_SIZE 4096
@@ -30,6 +43,8 @@
 #define SUCCESS 0
 #define TAMMAX 100
 
+#define SYMMETRIC_KEY_LENGTH 32
+#define CIPHER_BLOCK_SIZE 16
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
