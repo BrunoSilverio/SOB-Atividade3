67f9f6683b9fe0e4e70821ef9cdcd0515e055adc
guipernicone <guipernicone@hotmail.com>
2018-10-19 01:22:50 -0300

    Deu certo decripti

--- Projeto1Crypto/cryptomodule.c
+++ Projeto1Crypto/cryptomodule.c
@@ -41,14 +41,56 @@ e mandar a string com aspas duplas para o modulo*/
 module_param(key, charp, 0);
 MODULE_PARM_DESC(key, "A character string");
 
+static void shiftConcat(size_t const size, char *stringHex, char *stringNorm)
+{
+	unsigned char byte;
+	int i, j;
+
+	// Transformar em valores hexa normal
+	for(i = 0; i < size; i++){
+		pr_info("%i ", stringHex[i]);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((int)stringHex[i] >= 48 && (int)stringHex[i] <= 57)
+			stringHex[i] -= (char)48;
+		else if ((int)stringHex[i] >= 97 && (int)stringHex[i] <= 102)
+			stringHex[i] -= (char)87;
+		else if ((int)stringHex[i] >= 65 && (int)stringHex[i] <= 70)
+			stringHex[i] -= (char)55;
+		else
+			pr_info("ERROR: String em Hexa contem valor desconhecido -> %i", i);
+	}
+
+	// Concatenação em um unico byte
+	j = 0;
+	for (i = 0; i < size / 2; i++)
+	{
+		stringNorm[i] = (stringHex[j] << 4) + stringHex[j + 1];
+		j += 2;
+	}
+	// Para Printar -- Revomer depois
+	for (i = 0; i < size / 2; i++)
+	{
+		for (j = 7; j >= 0; j--)
+		{
+			byte = (stringNorm[i] >> j) & 1;
+			pr_info("%u", byte);
+		}
+		pr_info(" ");
+	}
+	pr_info("\n");
+}
 /* ---------------------------------------------------------------- */
 
-struct tcrypt_result{
+struct tcrypt_result
+{
 	struct completion completion;
 	int err;
 };
 
-struct skcipher_def{
+struct skcipher_def
+{
 	struct scatterlist sg;
 	struct crypto_skcipher *tfm;
 	struct skcipher_request *req;
@@ -60,7 +102,8 @@ struct skcipher_def{
 
 static struct skcipher_def sk;
 /*-------------------------------Encrypt---------------------------------------*/
-static void test_skcipher_finish(struct skcipher_def *sk){
+static void test_skcipher_finish(struct skcipher_def *sk)
+{
 	if (sk->tfm)
 		crypto_free_skcipher(sk->tfm);
 	if (sk->req)
@@ -72,31 +115,33 @@ static void test_skcipher_finish(struct skcipher_def *sk){
 	if (sk->ciphertext)
 		kfree(sk->ciphertext);
 }
-static int test_skcipher_result(struct skcipher_def *sk, int rc){
+static int test_skcipher_result(struct skcipher_def *sk, int rc)
+{
 	switch (rc)
 	{
-		case 0:
-			break;
-		case -EINPROGRESS:
+	case 0:
+		break;
+	case -EINPROGRESS:
 
-		case -EBUSY:
-			rc = wait_for_completion_interruptible(
-				&sk->result.completion);
-			if (!rc && !sk->result.err)
-			{
-				reinit_completion(&sk->result.completion);
-				break;
-			}
-		default:
-			pr_info("skcipher encrypt returned with %d result %d\n",
-					rc, sk->result.err);
+	case -EBUSY:
+		rc = wait_for_completion_interruptible(
+			&sk->result.completion);
+		if (!rc && !sk->result.err)
+		{
+			reinit_completion(&sk->result.completion);
 			break;
+		}
+	default:
+		pr_info("skcipher encrypt returned with %d result %d\n",
+				rc, sk->result.err);
+		break;
 	}
 
 	init_completion(&sk->result.completion);
 	return rc;
 }
-static void test_skcipher_callback(struct crypto_async_request *req, int error){
+static void test_skcipher_callback(struct crypto_async_request *req, int error)
+{
 	struct tcrypt_result *result = req->data;
 	int ret;
 	if (error == -EINPROGRESS)
@@ -106,7 +151,8 @@ static void test_skcipher_callback(struct crypto_async_request *req, int error){
 	pr_info("Encryption finished successfully result = %s\n", result->completion);
 }
 static int test_skcipher_encrypt(char *plaintext, char *password,
-								 struct skcipher_def *sk){
+								 struct skcipher_def *sk)
+{
 	int ret = -EFAULT;
 	unsigned char key[SYMMETRIC_KEY_LENGTH];
 
@@ -139,7 +185,6 @@ static int test_skcipher_encrypt(char *plaintext, char *password,
 
 	/* Use the world's favourite password */
 	sprintf((char *)key, "%s", password);
-
 
 	/* AES 256 with given symmetric key */
 	if (crypto_skcipher_setkey(sk->tfm, key, SYMMETRIC_KEY_LENGTH))
@@ -180,20 +225,107 @@ static int test_skcipher_encrypt(char *plaintext, char *password,
 	/* encrypt data */
 	ret = crypto_skcipher_encrypt(sk->req);
 
-	//ret = crypto_skcipher_decrypt(sk->req);//decripita o ciphertext dentro da scatterlist.
-	
+	//ret = crypto_skcipher_decrypt(sk->req); //decripita o ciphertext dentro da scatterlist.
+
 	ret = test_skcipher_result(sk, ret);
-	
+
 	if (ret)
 		goto out;
 	pr_info("Encryption request successful\n");
-	
+
 out:
 	return ret;
-
 }
 
-int cryptoapi_init(void){
+
+static int test_skcipher_dencrypt(char *plaintext, char *password,
+								 struct skcipher_def *sk)
+{
+	int ret = -EFAULT;
+	unsigned char key[SYMMETRIC_KEY_LENGTH];
+
+	if (!sk->tfm)
+	{
+		sk->tfm = crypto_alloc_skcipher("ecb(aes)", 0, 0);
+		if (IS_ERR(sk->tfm))
+		{
+			pr_info("could not allocate skcipher handle\n");
+			return PTR_ERR(sk->tfm);
+		}
+	}
+	if (!sk->req)
+	{
+		sk->req = skcipher_request_alloc(sk->tfm, GFP_KERNEL);
+		if (!sk->req)
+		{
+			pr_info("could not allocate skcipher request\n");
+			ret = -ENOMEM;
+			goto out;
+		}
+	}
+
+	skcipher_request_set_callback(sk->req, CRYPTO_TFM_REQ_MAY_BACKLOG,
+								  test_skcipher_callback,
+								  &sk->result);
+	/* clear the key */
+
+	memset((void *)key, '\0', SYMMETRIC_KEY_LENGTH);
+
+	/* Use the world's favourite password */
+	sprintf((char *)key, "%s", password);
+
+	/* AES 256 with given symmetric key */
+	if (crypto_skcipher_setkey(sk->tfm, key, SYMMETRIC_KEY_LENGTH))
+	{
+		pr_info("key could not be set\n");
+		ret = -EAGAIN;
+		goto out;
+	}
+	pr_info("Symmetric key: %s\n", key);
+	pr_info("Plaintext: %s\n", plaintext);
+	if (!sk->ivdata)
+	{
+		/* see https://en.wikipedia.org/wiki/Initialization_vector */
+		sk->ivdata = kmalloc(CIPHER_BLOCK_SIZE, GFP_KERNEL);
+		if (!sk->ivdata)
+		{
+			pr_info("could not allocate ivdata\n");
+			goto out;
+		}
+		get_random_bytes(sk->ivdata, CIPHER_BLOCK_SIZE);
+	}
+	if (!sk->scratchpad)
+	{
+		/* The text to be encrypted */
+		sk->scratchpad = kmalloc(CIPHER_BLOCK_SIZE, GFP_KERNEL);
+		if (!sk->scratchpad)
+		{
+			pr_info("could not allocate scratchpad\n");
+			goto out;
+		}
+	}
+	pr_info("Plaintext: %s\n", plaintext);
+	sprintf((char *)sk->scratchpad, "%s", plaintext);
+	sg_init_one(&sk->sg, sk->scratchpad, CIPHER_BLOCK_SIZE);
+	skcipher_request_set_crypt(sk->req, &sk->sg, &sk->sg,
+							   CIPHER_BLOCK_SIZE, sk->ivdata);
+	init_completion(&sk->result.completion);
+	/* encrypt data */
+
+	ret = crypto_skcipher_decrypt(sk->req); //decripita o ciphertext dentro da scatterlist.
+
+	ret = test_skcipher_result(sk, ret);
+
+	if (ret)
+		goto out;
+	pr_info("Encryption request successful\n");
+
+out:
+	return ret;
+}
+
+int cryptoapi_init(void)
+{
 
 	sk.tfm = NULL;
 	sk.req = NULL;
@@ -202,36 +334,46 @@ int cryptoapi_init(void){
 	sk.ivdata = NULL;
 
 	char *ciphertext;
+	char *deciphertext;
 	char resultado[BUF_LEN];
+	char resultado2[BUF_LEN];
+	char resultadoCerto[BUF_LEN];
 	int i;
-	test_skcipher_encrypt("bora da um role", key, &sk);
-  
-	
-	
+	test_skcipher_encrypt("Bora da um role", key, &sk);
+
 	//char *strAux;
 	//faz o calculo do endereco virtual utilizando o end de pagina e offset
 	ciphertext = sg_virt(&sk.sg);
 
-	
 	/*printa o conteudo do endereço ao utilizar a funcao 
 	* decrypt retorna o chipertext decripitado.*/
 
+	for (i = 0; i < strlen(ciphertext); i++)
+	{
 
-	for(i = 0; i < strlen(ciphertext); i++){
-	
-	  	sprintf(&resultado[i*2], "%02hhx", ciphertext[i]);
+		sprintf(&resultado[i * 2], "%02hhx", ciphertext[i]);
 		// strcat(resultado,strAux)
-    }
-	resultado[i*2] = '\0';
+	}
+	resultado[i * 2] = '\0';
 
 	pr_info("Resultado Cifrado: %s", resultado);
-	// for(i = 0; i < strlen(ciphertext); i++){
+
+	shiftConcat(strlen(resultado),resultado,resultadoCerto);
+
+	test_skcipher_dencrypt(resultadoCerto, key, &sk);
+
+	deciphertext = sg_virt(&sk.sg);
+
+
+	pr_info("Resultado decifrado: %s", deciphertext);
+
+	/*for(i = 0; i < strlen(ciphertext); i++){
 	
-	//    	pr_info("init encrypted : %02hhx , %i", ciphertext[i], i);
+	   	pr_info("init encrypted : %02hhx , %i", ciphertext[i], i);
 	
-    // }
+     }*/
 	//pr_info("init encrypted : %s", ciphertext);
-	
+
 	//test_skcipher_dencrypt(aux, key, &sk);
 
 	// ciphertext = sg_virt(&sk.sg);
@@ -239,67 +381,71 @@ int cryptoapi_init(void){
 	// pr_info("init dencrypted : %s", ciphertext);
 	return 0;
 }
-void cryptoapi_exit(void){
+void cryptoapi_exit(void)
+{
 	test_skcipher_finish(&sk);
 }
 /*-------------------------------Decrypt---------------------------------------*/
 
 /*-------------------------------HASH------------------------------------------*/
-static void show_hash_result ( char * plaintext , char * hash_sha256 ) { 
-    int i ; 
+static void show_hash_result(char *plaintext, char *hash_sha256)
+{
+	int i;
 
-    pr_info("sha256 test for string: '%s'\n", plaintext);
+	pr_info("sha256 test for string: '%s'\n", plaintext);
 
-    for (i = 0; i < SHA256_LENGTH; i++)
-        sprintf(&string_hash[i*2], "%02x", (unsigned char)hash_sha256[i]);
+	for (i = 0; i < SHA256_LENGTH; i++)
+		sprintf(&string_hash[i * 2], "%02x", (unsigned char)hash_sha256[i]);
 
-    string_hash[i * 2] = 0;
+	string_hash[i * 2] = 0;
 
-    pr_info("%s\n", string_hash);
+	pr_info("%s\n", string_hash);
 }
-int cryptosha256_init(char *plaintext){
+int cryptosha256_init(char *plaintext)
+{
 
-    char hash_sha256[SHA256_LENGTH];
+	char hash_sha256[SHA256_LENGTH];
 
-    struct crypto_shash *sha256;
+	struct crypto_shash *sha256;
 
-    struct shash_desc *shash;  //Make and install the module: And you should see that the hash was calculated for the test string. Finally, remove the test module: Symmetric key encryption Here is an example of symmetrically encrypting a string using the AES algorithm and a password. sha256 = crypto_alloc_shash( "sha256" , 0, 0);
-    
-	sha256 = crypto_alloc_shash("sha256",0,0);
+	struct shash_desc *shash; //Make and install the module: And you should see that the hash was calculated for the test string. Finally, remove the test module: Symmetric key encryption Here is an example of symmetrically encrypting a string using the AES algorithm and a password. sha256 = crypto_alloc_shash( "sha256" , 0, 0);
 
-    if (IS_ERR(sha256))
-        return -1;
+	sha256 = crypto_alloc_shash("sha256", 0, 0);
 
-    shash = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(sha256), GFP_KERNEL);
-   
-    if (!shash)
-        return -ENOMEM;
-    
-    shash->tfm = sha256;
-    shash->flags = 0;
-    
-    if (crypto_shash_init(shash))
-        return -1;
-    if (crypto_shash_update(shash, plaintext, strlen(plaintext)))
-        return -1;
-    if (crypto_shash_final(shash, hash_sha256))
-        return -1;
-  
-    kfree(shash);
+	if (IS_ERR(sha256))
+		return -1;
 
-    crypto_free_shash(sha256);
+	shash = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(sha256), GFP_KERNEL);
 
-    show_hash_result(plaintext, hash_sha256);
-	
-    return 0;
+	if (!shash)
+		return -ENOMEM;
+
+	shash->tfm = sha256;
+	shash->flags = 0;
+
+	if (crypto_shash_init(shash))
+		return -1;
+	if (crypto_shash_update(shash, plaintext, strlen(plaintext)))
+		return -1;
+	if (crypto_shash_final(shash, hash_sha256))
+		return -1;
+
+	kfree(shash);
+
+	crypto_free_shash(sha256);
+
+	show_hash_result(plaintext, hash_sha256);
+
+	return 0;
 }
 
 /*-------------------------------Funcoes de W/R------------------------------------------*/
-static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset){
-	
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset)
+{
+
 	int bytes_read = 0;
-	readMSG	= string_hash;
-	
+	readMSG = string_hash;
+
 	if (operacao == 'c' || operacao == 'C')
 	{
 		/*Retorna a o dado crifrado*/
@@ -313,15 +459,16 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
 		/*Retorna a o resumo criptografico*/
 
 		/*If we're at the end of the message,return 0 signifying end of file*/
-		
+
 		if (*readMSG == 0)
-        return 0;
+			return 0;
 
 		/*
      	* Actually put the data into the buffer
      	*/
-	
-		while (length && *readMSG) {
+
+		while (length && *readMSG)
+		{
 
 			/*
 			* The buffer is in the user data segment, not the kernel
@@ -329,39 +476,38 @@ static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_
 			* put_user which copies data from the kernel data segment to
 			* the user data segment.
 			*/
-		
+
 			put_user(*(readMSG++), buffer++);
 
 			length--;
 			bytes_read++;
-   		}
-		  
+		}
 	}
 	else
 	{
 		pr_info("Operacao invalida");
 	}
-	pr_info( "arquivo lido");
+	pr_info("arquivo lido");
 	return bytes_read;
 }
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off){
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off)
+{
 	int i, j;
-	char  tipo, msgPassada[BUF_LEN];
+	char tipo, msgPassada[BUF_LEN];
 	sprintf(msg, "%s", buff, len);
 	size_of_message = strlen(msg);
 	operacao = msg[0];
-	
+
 	pr_info("msg  = %s", msg);
 	pr_info("Operacao = %c", msg[0]);
 	pr_info("size_of_message = %i", size_of_message);
-	
 
-	for(i = 0;i < size_of_message - 2;i++)
+	for (i = 0; i < size_of_message - 2; i++)
 	{
 		msgPassada[i] = msg[i + 2];
 	}
 	msgPassada[i] = '\0';
-	pr_info("Msg recebida: %s",msgPassada);
+	pr_info("Msg recebida: %s", msgPassada);
 
 	if (operacao == 'c' || operacao == 'C')
 	{
@@ -376,7 +522,7 @@ static ssize_t device_write(struct file *filp, const char *buff, size_t len, lof
 	{
 		/*Resumo criptografico key*/
 		char *plaintext = "This is a test";
-	
+
 		cryptosha256_init(plaintext);
 	}
 	else
